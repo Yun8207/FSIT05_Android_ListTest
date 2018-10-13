20e2a3de3653c15548f733ace6b1970640b38328
Alex <alexs880121@gmail.com>
2018-10-13 11:30:10 +0800

    v2

--- app/src/main/java/tw/alex/listtest/MainActivity.java
+++ app/src/main/java/tw/alex/listtest/MainActivity.java
@@ -4,6 +4,9 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private SimpleAdapter simpleAdapter;
     private LinkedList<HashMap<String,Object>> data;
 
+    String[] from = {"title","content","img"};
+    int[] to  = {R.id.item_title,R.id.item_content,R.id.item_img};
+
     private int[] imgs = {R.drawable.img0, R.drawable.img1, R.drawable.img2, R.drawable.img3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListView(){
-        String[] from = {"title","content","img"};
-        int[] to  = {R.id.item_title,R.id.item_content,R.id.item_img};
+
 
         data = new LinkedList<>();
 
@@ -47,5 +52,41 @@ public class MainActivity extends AppCompatActivity {
         simpleAdapter = new SimpleAdapter(this,data,R.layout.item,from,to);
 
         listView.setAdapter(simpleAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                HashMap<String, Object> target = data.get(position);
+                Log.d("alex", "item:1");
+
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("alex", "item:2");
+                return true;
+            }
+        });
+
+
+
+    }
+
+    public void addItem(View view) {
+        HashMap<String,Object> row = new HashMap<>();
+        row.put(from[0], "new");
+        row.put(from[1], "content");
+        row.put(from[2], imgs[(int)(Math.random()*4)]);
+        data.add(0,row);
+
+        simpleAdapter.notifyDataSetChanged();// reset adapter
+
+    }
+
+    public void deleteItem(View view) {
+        data.remove(3);
+        simpleAdapter.notifyDataSetChanged();
     }
 }
