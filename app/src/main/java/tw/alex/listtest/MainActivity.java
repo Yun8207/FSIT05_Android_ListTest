170964cf0a88e0b3bb7ec43e032ae42f81b9b518
Alex <alexs880121@gmail.com>
2018-10-13 10:37:53 +0800

    v1

---
+++ app/src/main/java/tw/alex/listtest/MainActivity.java
@@ -0,0 +1,51 @@
+package tw.alex.listtest;
+
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private SimpleAdapter simpleAdapter;
+    private LinkedList<HashMap<String,Object>> data;
+
+    private int[] imgs = {R.drawable.img0, R.drawable.img1, R.drawable.img2, R.drawable.img3};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        initListView();
+    }
+
+    private void initListView(){
+        String[] from = {"title","content","img"};
+        int[] to  = {R.id.item_title,R.id.item_content,R.id.item_img};
+
+        data = new LinkedList<>();
+
+        for( int i=0; i<100; i++){
+            HashMap<String,Object> row = new HashMap<>();
+            row.put(from[0], "title" + i);
+            row.put(from[1], "content" + i);
+            row.put(from[2], imgs[i%4]);
+            data.add(row);
+        }
+
+
+
+
+
+        simpleAdapter = new SimpleAdapter(this,data,R.layout.item,from,to);
+
+        listView.setAdapter(simpleAdapter);
+    }
+}
